9871a403d0d78f36a1bdaf4802463fcd53c4a712
bosemb08 <bosemb08@gmail.com>
2019-09-12 12:39:13 +0530

    OddorEven Numbers

---
+++ org/test/OddorEvenNos.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class OddorEvenNos {
+	public static void main(String[] args) {
+		
+		int n=50;
+		for (int i = 1; i < n; i++) {
+			if(i%2==0)
+				System.out.println(i + " ");
+			
+		}
+	}
+
+}
